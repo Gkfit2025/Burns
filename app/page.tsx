e7c3abe3a984b6c397614716a118bbefecc85f15
Gkfit2025 <promotiongkf46@gmail.com>
2025-08-07 21:25:03 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'; 
 
 import { useState, useEffect, useRef } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
